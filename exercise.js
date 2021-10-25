ede8f8280f8a29eecb729ff2d9bd683027f0c2be
Sol <solcis.bio@gmail.com>
2021-10-25 10:54:43 +0200

    first exercise

---
+++ exercise.js
@@ -0,0 +1,12 @@
+let listaCompra = [
+  "manzana",
+  "fresas",
+  "limones",
+  "mermelada",
+  "aguacate",
+  "mangos",
+];
+
+for (let i = 0; i < listaCompra.length; i++) {
+  console.log(listaCompra[i]);
+}
